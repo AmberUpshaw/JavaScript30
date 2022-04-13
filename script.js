55e0d15ea7ddc584d14faa6735d09ab0a421b3ad
AmberUpshaw <amberlyjeri@gmail.com>
2022-04-13 15:10:38 +0000

    No delay for sound, changed audio current time to 0. Also setup removeTransition function to enabled highlight to disappear when key isn't pressed

--- script.js
+++ script.js
@@ -3,6 +3,22 @@
 window.addEventListener('keydown', function(e){
 	//Grabs corresponding audio element for specific key
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	//Grabs div with key class
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	
 	if(!audio) return; //Stop function from running
+	audio.currentTime = 0; //rewind to the start
 	audio.play();
+	
+	//add class of playing to key so that squares are highlighted 
+	key.classList.add('playing');
+	
 });
+
+function removeTransition(e){
+	if(e.propertyName !== 'transform') return; //skip it if it's not transform
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
